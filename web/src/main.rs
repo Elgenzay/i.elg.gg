5911a2238ce74c1f4b797728374d67620e27bc5a
Jason <jason@elg.gg>
2023-03-10 19:52:05 -0500

    Serve files & 404 redirect

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::{
+	fs::{relative, NamedFile},
+	response::Redirect,
+};
+
+use std::{
+	env,
+	path::{Path, PathBuf},
+};
+
+#[get("/<path..>")]
+pub async fn static_files(path: PathBuf) -> Option<NamedFile> {
+	NamedFile::open(Path::new(relative!("static")).join(path))
+		.await
+		.ok()
+}
+
+#[catch(404)]
+pub async fn not_found(req: &rocket::Request<'_>) -> Redirect {
+	let fallback =
+		env::var("FALLBACK_DOMAIN").expect("Missing environment variable: FALLBACK_DOMAIN");
+	let mut new_uri = format!("https://{}", fallback);
+	let path = PathBuf::from(req.uri().path().to_string())
+		.into_os_string()
+		.into_string()
+		.unwrap();
+	new_uri.push_str(&path[..]);
+	Redirect::to(new_uri)
+}
+
+#[launch]
+fn rocket() -> _ {
+	dotenvy::dotenv().ok();
+	rocket::build()
+		.mount("/", rocket::routes![static_files])
+		.register("/", catchers![not_found])
 }
