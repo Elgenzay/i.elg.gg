68b89ab7874b6a155e12b9c58741d9caf53bc0fd
Jason <jason@elg.gg>
2023-05-20 00:40:17 -0400

    favicon & 404

--- web/src/main.rs
+++ web/src/main.rs
@@ -4,7 +4,6 @@ use rocket::{
 	get,
 	http::CookieJar,
 	launch,
-	response::Redirect,
 };
 use std::{
 	env,
@@ -45,16 +44,10 @@ pub async fn static_files(path: PathBuf, cookies: &CookieJar<'_>) -> Option<Name
 }
 
 #[catch(404)]
-pub async fn not_found(req: &rocket::Request<'_>) -> Redirect {
-	let fallback =
-		env::var("FALLBACK_DOMAIN").expect("Missing environment variable: FALLBACK_DOMAIN");
-	let mut new_uri = format!("https://{}", fallback);
-	let path = PathBuf::from(req.uri().path().to_string())
-		.into_os_string()
-		.into_string()
-		.unwrap();
-	new_uri.push_str(&path[..]);
-	Redirect::to(new_uri)
+pub async fn not_found() -> NamedFile {
+	NamedFile::open(Path::new(relative!("static")).join("404.html"))
+		.await
+		.unwrap()
 }
 
 #[launch]
